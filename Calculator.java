c5ab4b2641b7cf27699ce78ba3c779bf36f32d32
amira <119695675+Marmar9999@users.noreply.github.com>
2024-07-08 23:42:06 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,10 @@
-//Tasks
+//Session 1 Tasks
 //1. Create a simple calculator program that prompts the user to enter two numbers. The program should then calculate and display the sum, difference, product, quotient, and remainder of the two numbers.
-
 //2. Develop a console application to collect faculty data from students. The application should prompt the user to enter the faculty name, last grade, last degree, number of subjects, and whether the student passed or not.
 
-
+//Session 2 Tasks
+//1-Calculator App Enhancement: Modify the calculator application to receive the operation input from the user and implement a switch-case structure to handle all possible operators.
+//2-Cart App Modification: Update the cart application to retrieve data from the user, and verify if the quantity of any item is zero. If the quantity is zero, set the item's availability status to false.
 
 import java.util.*;
 class Calculator{
